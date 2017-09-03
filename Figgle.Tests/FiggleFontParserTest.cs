18aaad225a89d3fbd7cf2c76e6c3c69b1e712ef3
Drew Noakes <git@drewnoakes.com>
2017-09-03 13:20:20 +0100

    Add unit test to parse all embedded fonts.

---
+++ Figgle.Tests/FiggleFontParserTest.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Figgle.Tests
+{
+    public sealed class FiggleFontParserTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public FiggleFontParserTest(ITestOutputHelper output) => _output = output;
+
+        [Fact]
+        public void ParseAllEmbeddedFonts()
+        {
+            using (var stream = typeof(FiggleFonts).GetTypeInfo().Assembly.GetManifestResourceStream("Figgle.Fonts.zip"))
+            using (var zip = new ZipArchive(stream, ZipArchiveMode.Read))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    _output.WriteLine($"Parsing: {entry.Name}");
+
+                    using (var entryStream = entry.Open())
+                        FiggleFontParser.Parse(entryStream);
+                }
+            }
+        }
+    }
+}

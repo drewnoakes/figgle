7da360909cf9776b487ba0e9313294310fef724b
Drew Noakes <git@drewnoakes.com>
2020-11-24 21:23:47 +1100

    Use C# 9 top level statements for console app

--- Figgle.Cmd/Program.cs
+++ Figgle.Cmd/Program.cs
@@ -1,30 +1,20 @@
 ﻿// Copyright Drew Noakes. Licensed under the Apache-2.0 license. See the LICENSE file for more details.
 
 using System;
+using Figgle;
 
-// ReSharper disable FunctionNeverReturns
-
-namespace Figgle.Cmd
+while (true)
 {
-    internal static class Program
+    Console.Write("Message: ");
+
+    var message = Console.ReadLine();
+
+    try
     {
-        private static void Main()
-        {
-            while (true)
-            {
-                Console.Write("Message: ");
-
-                var message = Console.ReadLine();
-
-                try
-                {
-                    Console.WriteLine(FiggleFonts.Standard.Render(message));
-                }
-                catch (Exception e)
-                {
-                    Console.Error.WriteLine($"Exception: {e}");
-                }
-            }
-        }
+        Console.WriteLine(FiggleFonts.Standard.Render(message));
+    }
+    catch (Exception e)
+    {
+        Console.Error.WriteLine($"Exception: {e}");
     }
 }

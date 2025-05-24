683524feae02239c0df16cc9368b63221160a606
Jonathan Ou-Yang <jonathan.ouyang@emerson.com>
2025-05-24 19:56:27 +0800

    Add support for explicitly specify a font name for source generation

---
+++ Figgle.Generator.AcceptanceTests/FiggleSourceGeneratorAcceptanceTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System;
+using Xunit;
+
+namespace Figgle.Generator.AcceptanceTests;
+
+[GenerateFiggleText("HelloWorld", "My External Font", "Hello World!")]
+public partial class FiggleSourceGeneratorAcceptanceTests
+{
+    [Fact]
+    public void HelloWorldTextWithExternalFontIsSourceGenerated_RenderedTextMatches()
+    {
+        string expectedText = """
+            ██╗  ██╗███████╗██╗     ██╗      ██████╗     ██╗    ██╗ ██████╗ ██████╗ ██╗     ██████╗ ██╗
+            ██║  ██║██╔════╝██║     ██║     ██╔═══██╗    ██║    ██║██╔═══██╗██╔══██╗██║     ██╔══██╗██║
+            ███████║█████╗  ██║     ██║     ██║   ██║    ██║ █╗ ██║██║   ██║██████╔╝██║     ██║  ██║██║
+            ██╔══██║██╔══╝  ██║     ██║     ██║   ██║    ██║███╗██║██║   ██║██╔══██╗██║     ██║  ██║╚═╝
+            ██║  ██║███████╗███████╗███████╗╚██████╔╝    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝██╗
+            ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝      ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝
+            """;
+
+        Assert.Equal(expectedText, HelloWorld.Trim(), NewlineIgnoreComparer.Instance);
+    }
+
+    private sealed class NewlineIgnoreComparer : IEqualityComparer<string>
+    {
+        public static NewlineIgnoreComparer Instance { get; } = new();
+
+        public bool Equals(string? x, string? y)
+        {
+            return StringComparer.Ordinal.Equals(Normalize(x), Normalize(y));
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return StringComparer.Ordinal.GetHashCode(Normalize(obj));
+        }
+
+        [return: NotNullIfNotNull("s")]
+        private static string? Normalize(string? s) => s?.Replace("\r", "");
+    }
+}
---
+++ Figgle.Generator.Tests/TestAnalyzerConfigOptions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright Drew Noakes. Licensed under the Apache-2.0 license. See the LICENSE file for more details.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Figgle.Generator.Tests;
+
+internal sealed class TestAnalyzerConfigOptions : AnalyzerConfigOptions
+{
+    public static readonly TestAnalyzerConfigOptions Empty
+        = new(ImmutableDictionary<string, string>.Empty);
+
+    private readonly ImmutableDictionary<string, string> _options;
+
+    public TestAnalyzerConfigOptions(
+        ImmutableDictionary<string, string> options)
+    {
+        _options = options;
+    }
+
+    public override IEnumerable<string> Keys => _options.Keys;
+
+    public override bool TryGetValue(
+        string key,
+        [NotNullWhen(true)] out string? value)
+    {
+        return _options.TryGetValue(key, out value);
+    }
+}

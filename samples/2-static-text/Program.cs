10bc6c58f4be1761cd3edc7b27c6a546ebc92369
Drew Noakes <git@drewnoakes.com>
2025-06-07 23:06:16 +1000

    Add sample projects and restructure solution
    Creates a sample project for each of the primary modes of operating with Figgle:
    
    - Dynamic text generation (the easy path)
    - Static text generation
    - Embedding specific fonts from the `Figgle.Fonts` package
    - Embedding specific fonts from files on disk
    
    Each sample project should work in isolation outside of this repo. We don't want our `Directory.Build.props` or `.editorconfig` files to apply to these projects. For that reason, we move source and test projcets into new `src/` and `tests/` folders.
    
    Also convert the solution from `.sln` to `.slnx`. Requires VS 17.14.

---
+++ samples/2-static-text/Program.cs
@@ -0,0 +1,8 @@
+﻿using Figgle;
+
+Console.WriteLine(MyAsciiBanners.Greeting);
+
+[GenerateFiggleText(memberName: "Greeting", fontName: "blocks", sourceText: "Hello, World!")]
+internal static partial class MyAsciiBanners
+{
+}
---
+++ samples/3-embed-font-from-package/Program.cs
@@ -0,0 +1,26 @@
+﻿using Figgle;
+
+while (true)
+{
+    Console.Write("Enter text to print: ");
+
+    string? message = Console.ReadLine();
+
+    if (message is null)
+        break;
+
+    Console.WriteLine($"Rendered with {nameof(MyFonts.ThreeDDiagonal)}");
+    Console.WriteLine();
+    Console.WriteLine(MyFonts.ThreeDDiagonal.Render(message));
+    Console.WriteLine();
+    Console.WriteLine($"Rendered with {nameof(MyFonts.Blocks)}");
+    Console.WriteLine();
+    Console.WriteLine(MyFonts.Blocks.Render(message));
+    Console.WriteLine();
+}
+
+[EmbedFiggleFont(memberName: "ThreeDDiagonal", fontName: "3d_diagonal")]
+[EmbedFiggleFont(memberName: "Blocks", fontName: "blocks")]
+internal static partial class MyFonts
+{
+}

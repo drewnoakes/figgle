50969693acbae6dcb5eb3e931538eac7cf33e324
Drew Noakes <git@drewnoakes.com>
2025-06-07 23:06:16 +1000

    Add sample projects and restructure solution
    Creates a sample project for each of the primary modes of operating with Figgle:
    
    - Dynamic text generation (the easy path)
    - Static text generation
    - Embedding specific fonts from the `Figgle.Fonts` package
    - Embedding specific fonts from files on disk
    
    Each sample project should work in isolation outside of this repo. We don't want our `Directory.Build.props` or `.editorconfig` files to apply to these projects. For that reason, we move source and test projcets into new `src/` and `tests/` folders.
    
    Also convert the solution from `.sln` to `.slnx`. Requires VS 17.14.

---
+++ samples/1-basics/Program.cs
@@ -0,0 +1,16 @@
+﻿using Figgle;
+using Figgle.Fonts;
+
+while (true)
+{
+    Console.Write("Enter text to print: ");
+
+    string? message = Console.ReadLine();
+
+    if (message is null)
+        break;
+
+    FiggleFont font = FiggleFonts.Standard;
+
+    Console.WriteLine(font.Render(message));
+}
---
+++ samples/4-embed-font-from-file/Program.cs
@@ -0,0 +1,18 @@
+﻿using Figgle;
+
+while (true)
+{
+    Console.Write("Enter text to print: ");
+
+    string? message = Console.ReadLine();
+
+    if (message is null)
+        break;
+
+    Console.WriteLine(MyFonts.Cosmic.Render(message));
+}
+
+[EmbedFiggleFont(memberName: "Cosmic", fontName: "cosmic")]
+internal static partial class MyFonts
+{
+}

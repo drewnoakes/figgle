5d993d6383fc03e32269ac6452ee3c555b2a125a
Jonathan Ou-yang <jonathan.ouyang@ni.com>
2025-06-07 23:38:28 +0800

    Generate sample text in the font property's documentation

--- Figgle.Fonts.Generator/FiggleFontGenerator.cs
+++ Figgle.Fonts.Generator/FiggleFontGenerator.cs
@@ -3,17 +3,29 @@
 using System;
 using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
+using System.Security;
 using System.Text;
 using Microsoft.CodeAnalysis;
 
 namespace Figgle.Fonts.Generator;
 
 [Generator(LanguageNames.CSharp)]
-public class FiggleFontGenerator : IIncrementalGenerator
+internal sealed class FiggleFontGenerator : IIncrementalGenerator
 {
+    private static readonly string[] _newLineCharacters = ["\r\n", "\n"];
+
+    private static readonly DiagnosticDescriptor _errorParsingFontFileDiagnostic = new(
+        "FIGGLE_IMPL001",
+        "Font Parsing Error",
+        "Failed to parse font '{0}'. Ensure it is a valid FIGlet font file.",
+        "FiggleFonts",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var fontsProvider = context.AdditionalTextsProvider
+        var fontMemberNamesProvider = context.AdditionalTextsProvider
             .Where(file => Path.GetFileName(file.Path).Equals("Aliases.csv", StringComparison.OrdinalIgnoreCase))
             .Select(static (file, cancellationToken) =>
             {
@@ -24,7 +36,7 @@ public class FiggleFontGenerator : IIncrementalGenerator
                 }
 
                 var fontInfos = ImmutableArray.CreateBuilder<FontInfo>();
-                var entries = csvFileContent.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries);
+                var entries = csvFileContent.Split(_newLineCharacters, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var entry in entries)
                 {
                     var components = entry.Split(',');
@@ -39,28 +51,63 @@ public class FiggleFontGenerator : IIncrementalGenerator
                 return fontInfos.ToImmutable();
             });
 
-        context.RegisterSourceOutput(fontsProvider, (context, fontInfos) =>
+        var parsedFontsProvider = context.AdditionalTextsProvider
+            .Where(file => file.Path.EndsWith(".flf", StringComparison.OrdinalIgnoreCase))
+            .Select(static (file, cancellationToken) =>
+            {
+                var fontContent = file.GetText(cancellationToken)?.ToString();
+                return new ParsedFont(
+                    Path.GetFileNameWithoutExtension(file.Path),
+                    fontContent is null ? null : FiggleFontParser.ParseString(fontContent));
+            })
+            .Collect();
+
+        context.RegisterSourceOutput(fontMemberNamesProvider.Combine(parsedFontsProvider), (context, pair) =>
         {
+            var fontInfos = pair.Left;
+            var parsedFonts = pair.Right.ToImmutableDictionary(
+                keySelector: f => f.Name,
+                elementSelector: f => f.Font);
+
+            foreach (var kvp in parsedFonts)
+            {
+                if (kvp.Value is null)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        _errorParsingFontFileDiagnostic,
+                        Location.None,
+                        kvp.Key));
+                }
+            }
+
             var source = $$"""
                 namespace Figgle.Fonts;
 
-                static partial class FiggleFonts
-                {
-                {{RenderFiggleFontProperties(fontInfos)}}
+                partial class FiggleFonts
+                {{{RenderFiggleFontProperties(fontInfos, parsedFonts)}}
                 }
                 """;
 
             context.AddSource("FiggleFonts.g.cs", source);
 
-            static string RenderFiggleFontProperties(ImmutableArray<FontInfo> fontInfos)
+            static string RenderFiggleFontProperties(
+                ImmutableArray<FontInfo> fontInfos,
+                ImmutableDictionary<string, FiggleFont?> parsedFonts)
             {
                 var builder = new StringBuilder(capacity: 4096);
                 var indentation = new string(' ', 4);
                 foreach (var fontInfo in fontInfos)
                 {
-                    builder.AppendLine($$"""
+                    builder.Append($$"""
+
+
                         {{indentation}}/// <summary>
-                        {{indentation}}/// Gets the FIGlet font with the name "{{fontInfo.FontName}}".
+                        {{indentation}}/// Obtains the <see cref="Figgle.FiggleFont" /> for the font name "{{fontInfo.FontName}}".
+                        {{indentation}}/// <example>
+                        {{indentation}}/// <code>
+                        {{indentation}}{{RenderSampleText(fontInfo.FontName, parsedFonts[fontInfo.FontName], indentation)}}
+                        {{indentation}}/// </code>
+                        {{indentation}}/// </example>
                         {{indentation}}/// </summary>
                         {{indentation}}public static FiggleFont {{fontInfo.MemberName}} => GetByName("{{fontInfo.FontName}}");
                         """);
@@ -68,8 +115,33 @@ public class FiggleFontGenerator : IIncrementalGenerator
 
                 return builder.ToString();
             }
+
+            static string RenderSampleText(string fontName, FiggleFont? font, string indentation)
+            {
+                if (font is null)
+                {
+                    return $"Failed to parse {fontName} into a {nameof(FiggleFont)}";
+                }
+
+                var renderedText = font.Render(fontName);
+
+                return string.Join(
+                    $"\r\n{indentation}",
+                    renderedText
+                        .Split(_newLineCharacters, StringSplitOptions.None)
+                        .Select(line => $"/// {EscapeXmlSpecialCharacters(line)}"));
+            }
+
+            static string EscapeXmlSpecialCharacters(string text)
+            {
+                // SecurityElement conveniently has Escape that escapes XML special
+                // characters, so we can just reuse it.
+                return SecurityElement.Escape(text);
+            }
         });
     }
 
     private sealed record FontInfo(string FontName, string MemberName);
+
+    private sealed record ParsedFont(string Name, FiggleFont? Font);
 }
